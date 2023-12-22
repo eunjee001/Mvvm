cc15bd05df77471de6806532bc98b338d5c6b765
eunkyoung99 <eunjee001@gmail.com>
2023-12-22 17:19:36 +0900

    코드 라인 정리

--- app/src/main/java/com/kkyoungs/mvvm/Database.kt
+++ app/src/main/java/com/kkyoungs/mvvm/Database.kt
@@ -1,10 +1,12 @@
 package com.kkyoungs.mvvm
 
 class Database {
+
     private val personList = ArrayList<Person>()
     var winner: String? = null
     private var databaseListener: DatabaseListener? = null
     private var instance: Database? = null
+
     init {
         // Model인 Database 생성
         personList.add(Person(0, "똥꼬"))
@@ -32,6 +34,7 @@ class Database {
          println("DatabaseListener 구현 객체 참조 변수 세팅 (arg1 : %s)" +databaseListener.javaClass.simpleName)
         this.databaseListener = databaseListener
     }
+
     fun getInstance(): Database? {
         // Model에 접근 할 수 있도록 DB 인스턴스 값 요청
         if (instance == null) {
@@ -39,9 +42,8 @@ class Database {
         }
         return instance
     }
+
     interface DatabaseListener {
         fun onChanged()
     }
-
-
 }
--- app/src/main/java/com/kkyoungs/mvvm/ViewModel.kt
+++ app/src/main/java/com/kkyoungs/mvvm/ViewModel.kt
@@ -2,9 +2,9 @@ package com.kkyoungs.mvvm
 
 import androidx.databinding.BaseObservable
 import com.kkyoungs.mvvm.Database.DatabaseListener
+
 class ViewModel(): BaseObservable() {
     private var database : Database?= null
-    var items : List<Person> = ArrayList()
     var winner : String ?= null
 
         /**
@@ -15,7 +15,6 @@ class ViewModel(): BaseObservable() {
             this.database = database
             this.database!!.setOnDatabaseListener(object : DatabaseListener {
                 // 리스너 실행
-
                 override fun onChanged() {
                     winner = null
                     winner = database.winner
@@ -24,13 +23,8 @@ class ViewModel(): BaseObservable() {
             })
         }
 
-
-
         fun getUser(){
             // dp에게 user (winner)을 달라고 요청
             database!!.user
         }
-
-
-
 }
